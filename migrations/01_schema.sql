c0c34410c594b636022057e6aa3488fdd7c5938a
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-25 03:01:53 +0000

    Added more seeds, revised issues with seeds not copying over

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -11,18 +11,17 @@ CREATE TABLE users (
 );
 
 CREATE TABLE properties (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  ---Seperated because its easier to read
+---seperated because its easier to read
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_cover_url VARCHAR(255) NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
