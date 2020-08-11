c4f412dda50a04e6446c4368cc848b7d6a14073a
Bryan's Mac <bryanpgomes@gmail.com>
2020-08-10 22:00:33 -0400

    Modified add property function to accept queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -149,7 +149,7 @@ const getAllProperties = function(options, limit = 10) {
   // minimum and maximum price
   if (options.minimum_price_per_night && options.maximum_prie_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
-    (queryParams.length === 2) ?
+    (queryParams.length === 1) ?
     (queryString += `WHERE cost_per_night >= ${queryParams.length - 1} AND cost_per_night <= ${queryParams.length}`) :
     (queryString += `AND cost_per_night >= ${queryParams.length - 1} AND cost_per_night <= ${queryParams.length}`)
   }
@@ -159,8 +159,8 @@ const getAllProperties = function(options, limit = 10) {
   GROUP BY properties.id
   `;
   if (options.minimum_rating) {
-    queryParams.push(options.minimum_rating);
-    queryString += `HAVING avg(property_reviews.rating) >= ${queryParams.length} `;
+    queryParams.push(options.minimum_rating)
+    queryString += `AND rating >= ${queryParams.length}`
   }
 
   queryParams.push(limit);
@@ -191,9 +191,48 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+      INSERT INTO properties (
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        parking_spaces,
+        numebr_of_bathrooms,
+        number_of_bedrooms,
+        country,
+        street,
+        city,
+        province,
+        post_code
+      )
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `;
+    const values = [
+      property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.parking_spaces,
+      property.numebr_of_bathrooms,
+      property.number_of_bedrooms,
+      property.country,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code
+    ];
+  return pool.query(queryString, values)
+  .then(res => {
+    return res.rows[0];
+  })
+  .catch(err => {
+    return console.log('Uh oh, query error:', err);
+  })
 }
 exports.addProperty = addProperty;
