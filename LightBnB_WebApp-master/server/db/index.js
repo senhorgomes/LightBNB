54ed9c7810f0a398e62e0f70383e86bdfcdb78d8
Bryan's Mac <bryanpgomes@gmail.com>
2020-08-10 22:16:17 -0400

    Modification complete

--- LightBnB_WebApp-master/server/db/index.js
+++ LightBnB_WebApp-master/server/db/index.js
@@ -5,3 +5,9 @@ const pool = new Pool({
   host: 'localhost',
   database: 'lightbnb'
 });
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  },
+}
